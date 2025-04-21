638ec5900783c45c8ee63fe63f2d7ad5e0b70a8b
Jorge Rodriguez Alonso <jorgerodriguezalonso20@gmail.com>
2025-04-21 11:13:10 +0200

    modificación estructura archivos y su respectivo cambio en el routes.php

--- web/routes.php
+++ web/routes.php
@@ -2,10 +2,10 @@
 
     return [
         '/' => 'controllers/home.php',
-        '/empresa' => 'controllers/empresa.php',
-        '/departamento' => 'controllers/departamento.php',
-        '/eliminar-departamento' => 'controllers/eliminar-departamento.php',
-        '/empleado' => 'controllers/empleado.php',
+        '/empresa' => 'controllers/Empresa/empresa.php',
+        '/departamento' => 'controllers/Departamento/departamento.php',
+        '/eliminar-departamento' => 'controllers/Departamento/eliminar-departamento.php',
+        '/empleado' => 'controllers/Empleado/empleado.php',
         '/mi-cuenta' => 'controllers/Session/mi-cuenta.php',
         '/cerrar-sesion' => 'controllers/Session/cerrar-sesion.php',
         '/calcular-nomina/empleado' => 'controllers/Nomina/calcular-nomina.php',
