74cceb919332c7bbf928a9e753bf1f5f093169b7
Jorge Rodriguez Alonso <159543991+Jorgerdzz@users.noreply.github.com>
2025-06-05 10:51:19 +0200

    Actualización head.php

--- web/views/partials/head.php
+++ web/views/partials/head.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>NominaCount</title>
-    <link rel="stylesheet" href="//cdn.datatables.net/2.2.2/css/dataTables.dataTables.min.css"> <!--DataTables CSS -->
+    <link rel="stylesheet" href="https://cdn.datatables.net/2.2.2/css/dataTables.dataTables.min.css"> <!--DataTables CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <link rel="stylesheet" href="<?= BASE_PATH; ?>/views/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="<?= BASE_PATH; ?>/views/css/style.css">
